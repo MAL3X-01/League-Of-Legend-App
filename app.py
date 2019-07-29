c06f4ccbb290a55cf651aa67daabaf987390c7ad
Marvin A Davila <marvind@Marvins-MBP.fios-router.home>
2019-07-29 17:59:42 -0400

    Change App Style

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ dbc.themes.YETI
 """
 
 external_stylesheets = [
-    dbc.themes.BOOTSTRAP,
+    dbc.themes.DARKLY,
     'https://use.fontawesome.com/releases/v5.9.0/css/all.css', # for social media icons
 ]
 
--- pages/index.py
+++ pages/index.py
@@ -33,7 +33,7 @@ column1 = dbc.Col(
 
             Emphasize how the app will benefit users. Don't emphasize the underlying technology.
 
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
+            ✅ TESTING RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
 
             ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
 
